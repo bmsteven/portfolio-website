f6f880514bf0a395531513c34ad9c3f4d9df1606
benedict <bmsteve96@gmail.com>
2020-05-23 01:31:34 +0300

    updates

--- portfolio/src/components/about/steps.js
+++ portfolio/src/components/about/steps.js
@@ -10,13 +10,25 @@ const Steps = () => {
           <h1>My Work Process</h1>
         </header>
         <div className='showcase'>
-          {steps.map((step) => (
-            <article key={step.id}>
-              <h2>{step.id}</h2>
-              <h1>{step.title}</h1>
-              <p>{step.description}</p>
-            </article>
-          ))}
+          <div className='col-1'>
+            {steps.slice(0,2).map((step) => (
+              <article key={step.id}>
+                <h2>{step.id}</h2>
+                <h1>{step.title}</h1>
+                <p>{step.description}</p>
+              </article>
+            ))}
+          </div>
+          <div class="col-2"></div>
+          <div className='col-3'>
+            {steps.slice(-2).map((step) => (
+              <article key={step.id}>
+                <h2>{step.id}</h2>
+                <h1>{step.title}</h1>
+                <p>{step.description}</p>
+              </article>
+            ))}
+          </div>
         </div>
       </div>
     </section>
