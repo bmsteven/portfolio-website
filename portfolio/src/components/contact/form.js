342568b02fce77e091b27c12f96efc4a358f9eba
benedict <bmsteve96@gmail.com>
2020-05-26 23:21:11 +0300

    made some changes in portfolio page

--- portfolio/src/components/contact/form.js
+++ portfolio/src/components/contact/form.js
@@ -1,12 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
+const GOOGLE_FORM_NAME_ID = "entry.505834097";
+const GOOGLE_FORM_SUBJECT_ID = "entry.1498597591";
+const GOOGLE_FORM_EMAIL_ID = "entry.2005966555";
+const GOOGLE_FORM_MSG_ID = "entry.1142465389";
+const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+const GOOGLE_FORM_ACTION =
+  "https://docs.google.com/forms/d/e/1FAIpQLSfiid4vwZN1a5ap1huRtoL0WhqMMHsow8GcCCnNtxJglG9CQQ/formResponse?";
 
 const Form = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // submitted = true
+  const [data, setData] = useState({
+    Name: "",
+    email: "",
+    subject: "",
+    msg: "",
+  });
+
+  const { Name, email, subject, msg } = data;
+
+  const [loading, setLoading] = useState(false);
+
+  const [errors, setErrors] = useState("");
+  const [success, setSuccess] = useState("");
+
+  // const [emailSent, setEmailSent] = useState(false);
+
+  const sendEmail = () => {
+    setLoading(true);
+    const formData = new FormData();
+    formData.append(GOOGLE_FORM_NAME_ID, Name);
+    formData.append(GOOGLE_FORM_SUBJECT_ID, subject);
+    formData.append(GOOGLE_FORM_EMAIL_ID, email);
+    formData.append(GOOGLE_FORM_MSG_ID, msg);
+
+    axios
+      .post(CORS_PROXY + GOOGLE_FORM_ACTION, formData)
+      .then(() => {
+        setSuccess("Email Sent, will get back to you ASAP");
+      })
+      .catch(() => {
+        setErrors("Failed to send the email, please try again");
+      });
   };
 
-  // https://docs.google.com/forms/d/e/1FAIpQLSfiid4vwZN1a5ap1huRtoL0WhqMMHsow8GcCCnNtxJglG9CQQ/viewform?usp=pp_url&entry.505834097=hj&entry.2005966555=hj&entry.1498597591=jk&entry.1142465389=kk
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+
+    setData({ ...data, [name]: value });
+    console.log(data);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    sendEmail();
+  };
+
+  console.log(errors, success, loading);
 
   return (
     <section className='form'>
@@ -23,6 +74,7 @@ const Form = () => {
             placeholder='Name'
             id='Name'
             name='Name'
+            onChange={(e) => handleChange(e)}
             required
           />
         </div>
@@ -33,6 +85,7 @@ const Form = () => {
             placeholder='Email'
             id='Email'
             name='Email'
+            onChange={(e) => handleChange(e)}
             required
           />
         </div>
@@ -43,6 +96,7 @@ const Form = () => {
             placeholder='Subject'
             id='Subject'
             name='Subject'
+            onChange={(e) => handleChange(e)}
           />
         </div>
         <div className='field-group'>
@@ -51,6 +105,7 @@ const Form = () => {
             placeholder='Write a message'
             id='msg'
             name='msg'
+            onChange={(e) => handleChange(e)}
             required
           />
         </div>
