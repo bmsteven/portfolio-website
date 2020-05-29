d2ede662f60647434e13b36bee0315044a3b2ccd
benedict <bmsteve96@gmail.com>
2020-05-29 22:22:14 +0300

    added react-lazy-image-loader

--- portfolio/src/projects/pages/index.js
+++ portfolio/src/projects/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import FlipMove from "react-flip-move";
+// import FlipMove from "react-flip-move";
+import LazyLoad from "react-lazy-load";
 
 import projects from "../../data/projects";
 
@@ -63,38 +64,40 @@ const Projects = () => {
             <>
               {filteredProjects.map((item) => (
                 // <FlipMove>
-                  <article key={item.id} className='project-container project'>
-                    {item.src.length > 0 && (
-                      <>
-                        <figure className='project-image'>
+                <article key={item.id} className='project-container project'>
+                  {item.src.length > 0 && (
+                    <>
+                      <figure className='project-image'>
+                        <LazyLoad
+                          debounce={false}
+                          offsetVertical={500}
+                        >
                           <img src={item.src} alt={item.name} />
-                        </figure>
-                        <figcaption className='project-info'>
-                          <div className='project-name'>
-                            <h2>{item.name}</h2>
-                          </div>
-                          <span>#{item.category[0]}</span>
-                          <div className='btns'>
-                            <Link
-                              to={`/portfolio/${item.slug}`}
-                              className='btn'
+                        </LazyLoad>
+                      </figure>
+                      <figcaption className='project-info'>
+                        <div className='project-name'>
+                          <h2>{item.name}</h2>
+                        </div>
+                        <span>#{item.category[0]}</span>
+                        <div className='btns'>
+                          <Link to={`/portfolio/${item.slug}`} className='btn'>
+                            <span>About</span>
+                          </Link>
+                          {item.demo && (
+                            <a
+                              href={item.demo}
+                              target='_blank'
+                              rel='noopener noreferrer'
                             >
-                              <span>About</span>
-                            </Link>
-                            {item.demo && (
-                              <a
-                                href={item.demo}
-                                target='_blank'
-                                rel='noopener noreferrer'
-                              >
-                                Live App
-                              </a>
-                            )}
-                          </div>
-                        </figcaption>
-                      </>
-                    )}
-                  </article>
+                              Live App
+                            </a>
+                          )}
+                        </div>
+                      </figcaption>
+                    </>
+                  )}
+                </article>
                 // </FlipMove>
               ))}
             </>
