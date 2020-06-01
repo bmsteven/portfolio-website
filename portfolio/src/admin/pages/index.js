aa5a525f866b36d4cc6186b8070a47f28fad5425
benedict <bmsteve96@gmail.com>
2020-06-02 02:55:37 +0300

    added logout button

--- portfolio/src/admin/pages/index.js
+++ portfolio/src/admin/pages/index.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-const Home = () => {
-    return (
-        <div className="container">
-            <h2>Hello from admin home</h2>
-        </div>
-    )
-}
+import { logout } from "../../redux/actions/auth";
 
-export default Home
+const Home = ({ logout }) => {
+  return (
+    <div className='container'>
+      <h2>Hello from admin home</h2>
+      <a href='#!' onClick={logout}>
+        Logout
+      </a>
+    </div>
+  );
+};
+
+Home.propTypes = {
+  logout: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({});
+export default connect(mapStateToProps, { logout })(Home);
