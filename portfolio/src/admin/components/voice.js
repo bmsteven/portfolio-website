e2b77a43f75b188812af34a3d14df334bace5334
benedict <bmsteve96@gmail.com>
2020-06-03 13:33:33 +0300

    added aside menu in the admin panel

---
+++ portfolio/src/admin/components/voice.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+// import Artyom from './artyom.js';
+
+// const Jarvis = new Artyom();
+// console.log(Jarvis.say("Hello World !"))
+// Jarvis.say("Hello World !");
+
+export default class Voice extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
--- portfolio/src/admin/pages/index.js
+++ portfolio/src/admin/pages/index.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Voice from "../components/voice"
 // import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 const Home = ({ logout }) => {
   return (
     <div className=''>
-      <h2>Hello from admin home</h2>
+      <h2>Hello Boss, Welcome back</h2>
+      <p>How are you today?</p>
+      <Voice />
     </div>
   );
 };
