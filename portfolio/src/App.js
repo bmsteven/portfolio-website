f8ad505ce46ef1648a91e68123704513646e45ce
Benedict Steven <48621248+bmsteven@users.noreply.github.com>
2021-05-24 10:27:15 +0300

    Update App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,17 +1,17 @@
 import React, {useEffect} from "react";
 import axios from "axios";
-import jwtDecode from "jwt-decode";
+// import jwtDecode from "jwt-decode";
 
 //redux
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import store from "./redux/store";
-import { LOGIN_SUCCESS } from "./redux/types";
-import { logout } from "./redux/actions/auth";
-import { getPosts } from "./redux/actions/index";
+// import store from "./redux/store";
+//import { LOGIN_SUCCESS } from "./redux/types";
+//import { logout } from "./redux/actions/auth";
+//import { getPosts } from "./redux/actions/index";
 
 //protected-route
-import AuthRoute from "./util/auth-route";
+//import AuthRoute from "./util/auth-route";
 
 //router
 import { Switch, Route } from "react-router-dom";
@@ -22,34 +22,31 @@ import About from "./pages/about";
 import Resume from "./pages/resume";
 import Portfolio from "./projects";
 import Contact from "./pages/contact";
-import Admin from "./admin";
-import Blog from "./blog/";
-import Login from "./pages/login";
+//import Admin from "./admin";
+//import Blog from "./blog/";
+//import Login from "./pages/login";
 import Error from "./pages/error";
 
 //styles
 import "./styles/styles.css";
 
 //setting baseUrl
-axios.defaults.baseURL =
-  "https://us-central1-bmsteven-4db5f.cloudfunctions.net/api";
+//axios.defaults.baseURL =
+ // "https://us-central1-bmsteven-4db5f.cloudfunctions.net/api";
 
-const token = localStorage.FBIdToken;
-if (token) {
-  const decodedToken = jwtDecode(token);
-  if (decodedToken.exp * 1000 < Date.now()) {
-    store.dispatch(logout());
-    window.location.href = "/login";
-  } else {
-    store.dispatch({ type: LOGIN_SUCCESS });
-    axios.defaults.headers.common["Authorization"] = token;
-  }
-}
+//const token = localStorage.FBIdToken;
+//if (token) {
+ // const decodedToken = jwtDecode(token);
+ // if (decodedToken.exp * 1000 < Date.now()) {
+  //  store.dispatch(logout());
+   // window.location.href = "/login";
+  //} else {
+   // store.dispatch({ type: LOGIN_SUCCESS });
+   // axios.defaults.headers.common["Authorization"] = token;
+  //}
+//}
 
-const App = ({ getPosts, ui: { darkMode } }) => {
-  useEffect(() => {
-    store.dispatch(getPosts);
-  }, [getPosts]);
+const App = ({ ui: { darkMode } }) => {
   return (
     <Route
       render={({ location }) => (
@@ -60,9 +57,9 @@ const App = ({ getPosts, ui: { darkMode } }) => {
             <Route exact path='/resume' component={Resume} key='resume' />
             <Route path='/portfolio' component={Portfolio} key='projects' />
             <Route exact path='/contact' component={Contact} key='contact' />
-            <Route path='/blog' component={Blog} key='blog' />
-            <Route exact path='/login' key='login' component={Login} />
-            <AuthRoute path='/admin' component={Admin} key='admin' />
+           // <Route path='/blog' component={Blog} key='blog' />
+           // <Route exact path='/login' key='login' component={Login} />
+           // <AuthRoute path='/admin' component={Admin} key='admin' />
             <Route component={Error} key='error' />
           </Switch>
         </div>
@@ -77,7 +74,6 @@ const mapStateToProps = (state) => ({
 
 App.propTypes = {
   ui: PropTypes.object.isRequired,
-  getPosts: PropTypes.func.isRequired,
 };
 
-export default connect(mapStateToProps, {getPosts})(App);
+export default connect(mapStateToProps, {})(App);
