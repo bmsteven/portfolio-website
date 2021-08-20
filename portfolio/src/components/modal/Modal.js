4dd68547e7cf060ba87546d15615115c577e5722
Benedict <bmsteve96@gmail.com>
2021-08-21 00:56:28 +0300

    updates

--- portfolio/src/components/modal/Modal.js
+++ portfolio/src/components/modal/Modal.js
@@ -8,8 +8,10 @@ const Modal = () => {
   const [details, setDetails] = useState(
     projects?.active
       ? projects?.list.find((el) => el.id === projects.active)
+      : gallery?.active === 0
+      ? gallery?.list[0]
       : gallery?.active
-      ? gallery?.list.find((el) => el.id === gallery.active)
+      ? gallery.list[gallery?.active]
       : project?.id
       ? project
       : {}
@@ -36,6 +38,8 @@ const Modal = () => {
     if (open) document.body.classList.add("no-scrolling")
     // return document.body.classList.remove("no-scrolling")
   }, [open])
+
+  console.log(details, !gallery?.active, gallery.active.length)
 
   return (
     <div
@@ -67,23 +71,25 @@ const Modal = () => {
               <div className={styles.backdrop} onClick={close} />
               <img src={details.src} alt="" onClick={toggleOpen} />
             </div>
-          ) : gallery?.active ? (
-            <div className={`${styles.galery}`}>
+          ) : gallery?.list.length > 0 ? (
+            <div className={`${styles.gallery} ${styles.item}`}>
               <div className={styles.backdrop} onClick={close} />
-              <img src={details.src} alt="" />
+              <img src={details} alt="" />
             </div>
           ) : (
             <div></div>
           )}
-          <div
-            className={
-              isOpen && !gallery.active
-                ? `${styles.details} ${styles.open}`
-                : `${styles.details}`
-            }
-          >
-            {details.name}
-          </div>
+          {!gallery?.list.length > 0 && (
+            <div
+              className={
+                isOpen
+                  ? `${styles.details} ${styles.open}`
+                  : `${styles.details}`
+              }
+            >
+              {details.name}
+            </div>
+          )}
         </div>
       </div>
     </div>
